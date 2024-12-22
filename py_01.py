c18a23b25555def655cc9247bae619559816c360
parksungsu <parkss1324@naver.com>
2024-12-22 15:58:38 +0900

    printf

---
+++ py_01.py
@@ -0,0 +1,19 @@
+print("Hello world!")
+
+print("%d", 100)
+
+print("100")
+
+print("%d" % 100)
+
+print("100 + 100")
+
+print("%d" % (100 + 100))
+
+print("%d / %d = %5.1f" (100, 200, 0.5))
+
+print("%d / %d = %5.1f" % (100, 200, 0.5))
+
+print("%d / %d =%5.1f" % (100, 200, 0.5))
+
+print("%10s" % "Python")
