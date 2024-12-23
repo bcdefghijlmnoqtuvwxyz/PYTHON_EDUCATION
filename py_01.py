82b16d803a9100d57821a36997f40eff1e02fc5d
bcdefghijlmnoqtuvwxyz <parkss1324@naver.com>
2024-12-23 09:17:40 +0900

    코드 수정

--- py_01.py
+++ py_01.py
@@ -1,7 +1,5 @@
 print("Hello world!")
 
-print("%d", 100)
-
 print("100")
 
 print("%d" % 100)
@@ -10,10 +8,6 @@ print("100 + 100")
 
 print("%d" % (100 + 100))
 
-print("%d / %d = %5.1f" (100, 200, 0.5))
-
 print("%d / %d = %5.1f" % (100, 200, 0.5))
-
-print("%d / %d =%5.1f" % (100, 200, 0.5))
 
 print("%10s" % "Python")
